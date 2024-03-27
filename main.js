a572f7955d95ca8d44213235f91cae60c477e249
Pasquale Abate <lino.abate@gmail.com>
2024-03-27 17:33:36 +0100

    v.1.2

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 let navbar = document.querySelector('#navbar');
 let links = document.querySelectorAll('.nav-link');
 
-
-function changeNavbar( background, color2 , color3  ) {
-    navbar.classList.add(background);
-
-        links.forEach((link)=>{
-            link.addEventListener("mouseenter", ()=>{
-                link.style.borderBottom = color2;
-            });
-            link.addEventListener("mouseleave", ()=>{
-                link.style.borderBottom = color3;
-            });
+function navHover(color1,color2){
+    links.forEach((link)=>{
+        link.addEventListener("mouseenter", ()=>{
+            link.style.borderBottom = color1;
         });
+        link.addEventListener("mouseleave", ()=>{
+            link.style.borderBottom = color2;
+        });
+    });
 };
 
+function changeNavbar( background,) {
+    navbar.classList.add(background);
+};
+
+navHover("2px solid var(--Orange)","transparent");
 
 window.addEventListener("scroll", ()=>{
     let scrolled = window.scrollY;
 
     if (scrolled > 0) {
-        changeNavbar('nav-blur', "2px solid var(--Orange)" , "transparent");
+        changeNavbar('nav-blur');
     }else{
         navbar.classList.remove('nav-blur');
-        changeNavbar('nav-custom', "2px solid var(--Orange)" , "transparent");
+        changeNavbar('nav-custom');
     };
 
 });
