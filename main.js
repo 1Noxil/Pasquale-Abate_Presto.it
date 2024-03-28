d61ef76eefc7b672170414d5a8485eafe2e19114
Pasquale Abate <lino.abate@gmail.com>
2024-03-28 21:22:43 +0100

    versione mobile da finire

--- main.js
+++ main.js
@@ -20,14 +20,14 @@ navHover("2px solid var(--Orange)","transparent");
 
 window.addEventListener("scroll", ()=>{
     let scrolled = window.scrollY;
-
+    
     if (scrolled > 0) {
         changeNavbar('nav-blur');
     }else{
         navbar.classList.remove('nav-blur');
         changeNavbar('nav-custom');
     };
-
+    
 });
 
 /* Feedback section */
@@ -60,3 +60,72 @@ let observer = new IntersectionObserver((entries)=>{
 });
 
 observer.observe(firstnumber);
+
+
+// JS SWIPER
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// JS RECENSIONI
+
+let userName = document.querySelector('#userName');
+let userSubtitle = document.querySelector('#userSubtitle');
+let userReview = document.querySelector('#userReview');
+let btnReview = document.querySelector('#btnReview');
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+let sectionreviews = {
+    'reviews': [
+        {'name': "Elia" , 'title': "La mia piu' bella esperienza", 'description': "bellissima esperienza personale gentile, preparato, rapido , prezzi onestissimi" },
+        {'name': "Vincinzo" , 'title': "Pessima esperienza", 'description': "sito digustoso e poco professionale" },
+        {'name': "Laura" , 'title': "Esperienza COOL", 'description': "Accoglienza top, qualita' prodotti eccellente, lenta la spedizione " },
+        {'name': "Stefano" , 'title': "Bello", 'description': "TOP!! :) " },
+        {'name': "Giammarco" , 'title': "Altrove si trova di meglio", 'description': "le scarpe si sono rotte dopo 2 giorni! SCONSIGLIATO :(" },
+        {'name': "Rocco" , 'title': "Bellissima esperienza", 'description': "Abbiamo mangiato bene ma i camerieri non sono stati professionali, comodo per il mordi e fuggi" },
+    ],
+
+    'showRev':function(){
+        swiperWrapper.innerHTML= '';
+        this.reviews.forEach((review) => {
+            let div = document.createElement('div');
+            div.classList.add('swiper-slide');
+            div.innerHTML = `
+            <div class="title" data-swiper-parallax="-300">${review.name}</div>
+            <div class="subtitle" data-swiper-parallax="-200">${review.title}</div>
+            <div class="text" data-swiper-parallax="-100">
+            <p>“
+                ${review.description}”
+            </p>
+            </div>
+            `
+            swiperWrapper.appendChild(div);
+        });
+        swiper.update();
+    },
+    'addRev':function(){
+            this.reviews.push({'name': userName.value , 'title': userSubtitle.value, 'description': userReview.value },);
+    },
+ }
+
+
+sectionreviews.showRev();
+
+btnReview.addEventListener('click', () => {
+    sectionreviews.addRev();
+    sectionreviews.showRev();
+    userName.value = '';
+    userSubtitle.value= '';
+    userReview.value= '';
+    console.log(sectionreviews.reviews);
+    swiper.update();
+})
